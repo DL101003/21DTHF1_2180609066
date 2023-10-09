4f872b403e73e0db2da1ff9ed310bc000aacc8e9
vanba <vanba@DESKTOP-5TCML52>
2023-10-09 10:35:46 +0700

    Add project files.

---
+++ Lab5/Form1.cs
@@ -0,0 +1,43 @@
+﻿using Lab5.Model;
+using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Model1 context = new Model1();
+            List<Student> listStudent = context.Students.ToList();
+            List<StudentRp> listReport = new List<StudentRp>();
+            foreach (Student i in listStudent)
+            {
+                StudentRp temp = new StudentRp();
+                temp.StudentID = i.StudentID;
+                temp.FullName = i.FullName;
+                temp.AverageScore = i.AverageScore;
+                temp.FacultyName = i.Faculty.FacultyName;
+                listReport.Add(temp);
+            }
+            this.reportViewer1.LocalReport.ReportPath = "Report1.rdlc";
+            var reportDataSource = new ReportDataSource("DataSet1", listReport);
+            this.reportViewer1.LocalReport.DataSources.Clear();
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource);
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
